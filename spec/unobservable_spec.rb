063dce68a6bff84723025b35d564aac0887138de
Brian Lauber <constructile.truth@gmail.com>
2012-12-09 16:01:19 -0500

    Added test cases for the Unobservable#collect_instance_events_defined_by method.

--- spec/unobservable_spec.rb
+++ spec/unobservable_spec.rb
@@ -67,7 +67,23 @@ describe Class do
 end
 
 
+
 describe Unobservable do
+
+  def module_with_instance_events(*names)
+    Module.new do
+      include Unobservable::Support
+      attr_event *names
+    end
+  end
+
+  def class_with_instance_events(*names)
+    Class.new do
+      include Unobservable::Support
+      attr_event *names
+    end
+  end
+
 
   describe "#instance_events_for" do
     
@@ -75,6 +91,80 @@ describe Unobservable do
       expect{ Unobservable.instance_events_for(Object.new) }.to raise_error(TypeError)
     end
     
+  end
+  
+  
+  describe "#collect_instance_events_defined_by" do
+    let(:mixin_module) do
+      Module.new do
+        include Unobservable::Support
+        attr_event :mixin_1, :mixin_2
+      end
+    end
+    
+    let(:module_that_includes_mixin) do
+      m = mixin_module
+      
+      Module.new do
+        include m
+        include Unobservable::Support
+        attr_event :module_1, :module_2
+      end
+    end
+    
+    let(:baseclass) do
+      Class.new do
+        include Unobservable::Support
+        attr_event :bc_1, :bc_2
+      end
+    end
+    
+    let(:subclass) do
+      m = module_that_includes_mixin
+
+      Class.new(baseclass) do
+        include m
+        attr_event :sc_1, :sc_2
+      end
+    end
+    
+    
+    it "only collects the instance events that the contributors define explicitly" do
+      events = Unobservable::collect_instance_events_defined_by([subclass])
+      events.size.should == 2
+      events.should include(:sc_1)
+      events.should include(:sc_2)
+    end
+    
+    it "collects the instance events defined by each contributor" do
+      classes = (1..3).map{|i| class_with_instance_events("a#{i}", "b#{i}") }
+      modules = (1..3).map{|i| module_with_instance_events("c#{i}", "d#{i}") }
+      
+      events = Unobservable::collect_instance_events_defined_by(classes + modules)
+      events.size.should == 12
+      (1..3).each do |i|
+        events.should include("a#{i}".to_sym)
+        events.should include("b#{i}".to_sym)
+        events.should include("c#{i}".to_sym)
+        events.should include("d#{i}".to_sym)
+      end
+    end
+    
+    
+    it "does not repeat duplicate instance events" do
+      c1 = class_with_instance_events(:one, :two)
+      c2 = class_with_instance_events(:one, :three)
+      m1 = module_with_instance_events(:two, :three)
+      m2 = module_with_instance_events(:two)
+      
+      events = Unobservable::collect_instance_events_defined_by([c1, c2, m1, m2])
+      events.size.should == 3
+      events.should include(:one)
+      events.should include(:two)
+      events.should include(:three)
+    end
+
+
   end
   
 end
