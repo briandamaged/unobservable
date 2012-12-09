d518d980c324a10b20855e059de02ee1c5eceb5b
Brian Lauber <constructile.truth@gmail.com>
2012-12-09 14:58:51 -0500

    Moved more test case logic into a shared example group.

--- spec/unobservable_spec.rb
+++ spec/unobservable_spec.rb
@@ -9,8 +9,8 @@ shared_examples_for "instance event container" do
       Unobservable::instance_events_for(c, true).should be_empty
       Unobservable::instance_events_for(c, false).should be_empty
     end
-    
   end
+  
   
   context "Unobservable::Support is included" do
 
@@ -21,7 +21,7 @@ shared_examples_for "instance event container" do
       end
     end
     
-    let(:c) do
+    let(:instance_event_container) do
       m = mixin_module
       described_class.new do
         include Unobservable::Support
@@ -36,9 +36,16 @@ shared_examples_for "instance event container" do
       Unobservable::instance_events_for(c, true).should be_empty
       Unobservable::instance_events_for(c, false).should be_empty
     end
+    
+    it "knows which events have been defined explicitly" do
+      events = Unobservable::instance_events_for(instance_event_container, false)
+      events.size.should == 2
+      events.should include(:three)
+      events.should include(:four)
+    end
 
     it "inherits instance events defined by included Modules" do
-      events = Unobservable::instance_events_for(c, true)
+      events = Unobservable::instance_events_for(instance_event_container, true)
       events.size.should == 4
       events.should include(:one)
       events.should include(:two)
@@ -68,48 +75,7 @@ describe Unobservable do
       expect{ Unobservable.instance_events_for(Object.new) }.to raise_error(TypeError)
     end
     
-
-
-
-        
-    context "Module defines instance events" do
-      let(:mixin_module) do
-        Module.new do
-          include Unobservable::Support
-          attr_event :one, :two
-        end
-      end
-      
-      let(:module_with_events) do
-        # Due to scoping weirdness, we need to place the mixin module
-        # in a local variable first.
-        m = mixin_module
-        Module.new do
-          include Unobservable::Support
-          include m
-          attr_event :three, :four
-        end
-      end
-    
-      it "returns instance events defined explicitly by the Module when all=false" do
-        events = Unobservable::instance_events_for(module_with_events, false)
-        events.size.should == 2
-        events.should include(:three)
-        events.should include(:four)
-      end
-      
-      
-      it "returns instance events defined explicitly and through included Modules when all=true" do
-        events = Unobservable::instance_events_for(module_with_events, true)
-        events.size.should == 4
-        events.should include(:one)
-        events.should include(:two)
-        events.should include(:three)
-        events.should include(:four)
-      end
-      
-    end
-
   end
   
 end
+
