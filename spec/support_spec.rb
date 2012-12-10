11e4be9af2cbe340e4494a1e52ffcb9e49150fe7
Brian Lauber <constructile.truth@gmail.com>
2012-12-09 22:16:21 -0500

    Added more test cases around Unobservable::Support#event

--- spec/support_spec.rb
+++ spec/support_spec.rb
@@ -4,7 +4,8 @@ include Unobservable::SpecHelper
 
 module Unobservable
   describe Support do
-    let(:x) { class_with_instance_events(:foo, :bar).new }
+    let(:my_class) { class_with_instance_events(:foo, :bar) }
+    let(:obj) { my_class.new }
 
     context "when included by a module" do
       it "causes the module to extend Unobservable::ModuleSupport" do
@@ -25,38 +26,42 @@ module Unobservable
         x.singleton_class.singleton_class.should include(Unobservable::ModuleSupport)
       end
     end
-    
+
     
     describe "#event" do
       it "raises a NameError when the specified event is not defined" do
-        expect{ x.event(:not_exist) }.to raise_error(NameError)
+        expect{ obj.event(:not_exist) }.to raise_error(NameError)
       end
       
       it "maps different names to different Event instances" do
-        x.event(:foo).should_not === x.event(:bar)
+        obj.event(:foo).should_not === obj.event(:bar)
       end
       
       it "maps the same name to the same Event instance" do
-        x.event(:foo).should === x.event(:foo)
-        x.event(:bar).should === x.event(:bar)
+        obj.event(:foo).should === obj.event(:foo)
+        obj.event(:bar).should === obj.event(:bar)
       end
       
-      
+      it "maintains separate Events for each object" do
+        x = my_class.new
+        y = my_class.new
+        
+        x.event(:foo).should_not === y.event(:foo)
+      end
     end
     
     
     describe "#raise_event" do
       
-      
       it "raises a NameError when the specified event is not defined" do
-        expect{ x.send :raise_event, :not_exist }.to raise_error(NameError)
-        expect{ x.send :raise_event, :not_exist, 1, 2, 3 }.to raise_error(NameError)
+        expect{ obj.send :raise_event, :not_exist }.to raise_error(NameError)
+        expect{ obj.send :raise_event, :not_exist, 1, 2, 3 }.to raise_error(NameError)
       end
             
       it "calls the specified event" do
         args = [1, 2, 3]
-        x.event(:foo).should_receive(:call).with(*args)
-        x.send :raise_event, :foo, *args
+        obj.event(:foo).should_receive(:call).with(*args)
+        obj.send :raise_event, :foo, *args
       end
       
     end
